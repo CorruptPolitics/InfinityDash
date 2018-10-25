b92a0cbc01936c46c01bee57fc68cfd166fdf6a1
Kaelan Gillick <ktgillick@gmail.com>
2018-10-25 17:14:12 -0400

    movement ans start to death
    -Changed Movement variables to be more responsive and make speeding up less frustrating
    -Added a game manager game object and a script attached to that in order to pull the start poisiotn and keep track of the position of player and platform generator, more work still needs to be done 6:36 in tutorial ep 11

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    //declares the platform generator object
+    public Transform platformGenerator;
+    //declares where the platform generator begins
+    private Vector3 platformStartPoint;
+    //declares the player as the PlayerMovement script
+    public PlayerMovement thePlayer;
+    //declares the point at which the player starts
+    private Vector3 playerStartPoint;
+	
+    
+    // Use this for initialization
+	void Start () {
+    //sets the platform start point to the original position of the platform generator game object
+        platformStartPoint = platformGenerator.position;
+    //sets the player start point to the original position of the player game object
+        playerStartPoint = thePlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
