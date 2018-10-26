f8f5d76bd97f3d186503d5e07d8e240c803e0af3
Kaelan Gillick <ktgillick@gmail.com>
2018-10-26 15:08:36 -0400

    restart function progress
    Created the restart function with delay and deactiviating the player gameobject.  Haven't referenced it yet so still not working. 12 minutes in tutorial

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
     //declares the platform generator object
     public Transform platformGenerator;
     //declares where the platform generator begins
@@ -11,18 +12,41 @@ public class GameManager : MonoBehaviour {
     public PlayerMovement thePlayer;
     //declares the point at which the player starts
     private Vector3 playerStartPoint;
-	
-    
+
+
     // Use this for initialization
-	void Start () {
-    //sets the platform start point to the original position of the platform generator game object
+    void Start()
+    {
+        //sets the platform start point to the original position of the platform generator game object
         platformStartPoint = platformGenerator.position;
-    //sets the player start point to the original position of the player game object
+        //sets the player start point to the original position of the player game object
         playerStartPoint = thePlayer.transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //call our co-routine
+    public void RestartGame()
+    {
+        StartCoroutine("RestartGameCo");
+    }
+    //declare co-routine named RestartGameCo using IEnumerator
+    public IEnumerator RestartGameCo()
+    {
+        //makes the player invisible while "dead"
+        thePlayer.gameObject.SetActive(false);
+        //sets a hlaf second delay before game restarts
+        yield return new WaitForSeconds(0.5f);
+        //sets the players transform position back to the player start point
+        thePlayer.transform.position = playerStartPoint;
+        //sets the platformGenerator's position back to the platform start point
+        platformGenerator.position = platformStartPoint;
+        //turns the player visible again
+        thePlayer.gameObject.SetActive(true);
+    }
+
 }
