c48f73d48bbfdd18c8d74a02065d796f34a5071b
Zack Ziskin <36832530+IZackMan12@users.noreply.github.com>
2019-02-21 15:36:10 -0500

    Music in the game!
    Music in the game.

---
+++ Assets/Scripts/MusicSwitcher.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicSwitcher : MonoBehaviour
+{
+    Button MusicButton;
+    public GameObject hackButton;
+    public GameObject heatButton;
+    public GameObject raceButton;
+    public AudioSource audioManager;
+    public AudioClip hackMusic;
+    public AudioClip heatSeekMusic;
+    public AudioClip raceMusic;
+
+    void Start()
+    {
+        //Fetch the Dropdown GameObject
+        MusicButton = GetComponent<Button>();
+        hackButton.SetActive(false);
+        heatButton.SetActive(false);
+        raceButton.SetActive(false);
+    }
+
+    public void OpenMusicClips()
+    {
+        hackButton.SetActive(true);
+        heatButton.SetActive(true);
+        raceButton.SetActive(true);
+    }
+
+    public void StartHackMusic()
+    {
+        audioManager.clip = hackMusic;
+        audioManager.Play();
+        hackButton.SetActive(false);
+        heatButton.SetActive(false);
+        raceButton.SetActive(false);
+    }
+
+    public void StartHeatSeeker()
+    {
+        audioManager.clip = heatSeekMusic;
+        audioManager.Play();
+        hackButton.SetActive(false);
+        heatButton.SetActive(false);
+        raceButton.SetActive(false);
+    }
+
+    public void StartRaceMusic()
+    {
+        audioManager.clip = raceMusic;
+        audioManager.Play();
+        hackButton.SetActive(false);
+        heatButton.SetActive(false);
+        raceButton.SetActive(false);
+    }
+}
