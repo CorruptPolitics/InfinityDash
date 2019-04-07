098ce55d8d82f7c875a843d11b52474378fb5b76
Zack Ziskin <36832530+IZackMan12@users.noreply.github.com>
2019-04-06 21:45:36 -0400

    Parallaxing Script Added.
    Added a script for parallaxing backgrounds.  Once we get some art backgrounds in, we can have different depth of field for backgrounds.  i will probably make a city theme or something not sure.

---
+++ Assets/Scripts/Parallaxing.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour{ 
+
+    //Array of all back and foregrounds to be parallaxed.
+    public Transform[] backGrounds;
+    //Array of all the back and foreground scales relative to camera movement.
+    private float[] parallaxScale;
+    //How smooth the parallaxing is.
+    public float smoothing = 1f;
+    //Reference to main camera.
+    private Transform cam;
+    //Store posisiton of camera in previous frame.
+    private Vector3 previousCamPos;
+
+    void Awake()
+    {
+        cam = Camera.main.transform; 
+    }
+
+    void Start()
+    {
+        //Previous frame had current frame's camera position.
+        previousCamPos = cam.position;
+        //Making sure parallax array is same size as backgrounds array.
+        parallaxScale = new float[backGrounds.Length];
+        //Assigning correct parallax scale to correct background.
+        for (int i = 0; i < backGrounds.Length; i++)
+        {
+            parallaxScale[i] = backGrounds[i].position.z * -1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Loop through each background.
+        for (int i = 0; i < backGrounds.Length; i++)
+        {
+            //Difference between camera is from current frame from last frame.  Then it's applied to appropriate scale.
+            float parallax = previousCamPos.x - cam.position.x * parallaxScale[i];
+            //New x target position which is the current postiion plus the parallax.
+            float backgroundTargetPosX = backGrounds[i].position.x + parallax;
+            //Create target position which is current position with target x position.
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backGrounds[i].position.y, backGrounds[i].position.z);
+            //Lerp between current position and the target position.
+            backGrounds[i].position = Vector3.Lerp(backGrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+
+        //Set previous camera's position to camera's position at end of frame.
+        previousCamPos = cam.position;
+    }
+}
